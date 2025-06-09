e4049bfb945da2c90aef1ba1c3e49b7a72bd1aa2
Hemanth Kumar Nannuri <nannurihk007@gmail.com>
2025-06-09 15:51:42 -0500

    Update STIG-ID-WN10-UR-000160-Remediation.ps1

--- STIG-ID-WN10-UR-000160-Remediation.ps1
+++ STIG-ID-WN10-UR-000160-Remediation.ps1
@@ -1,29 +1,3 @@
-<#
-.SYNOPSIS
-    This PowerShell script ensures that The Restore files and directories user right must only be assigned to the Administrators group. Inappropriate granting of user rights can provide system, administrative, and other high level capabilities. Accounts with the "Restore files and directories" user right can circumvent file and directory permissions and could allow access to sensitive data. It could also be used to over-write more current data.
-.NOTES
-    Author          : Carlton Hurd
-    LinkedIn        : linkedin.com/in/carlton-hurd-6069a5120/
-    GitHub          : github.com/churd-git
-    Date Created    : 2025-01-14
-    Last Modified   : 2025-01-14
-    Version         : 1.0
-    CVEs            : N/A
-    Plugin IDs      : N/A
-    STIG-ID         : WN10-UR-000160
-
-.TESTED ON
-    Date(s) Tested  : 2025-01-14
-    Tested By       : Carlton Hurd
-    Systems Tested  : 
-    PowerShell Ver. : 
-
-.USAGE
-    Please download the script and execute as administrator. 
-    Example syntax:
-    PS C:\> .\STIG-ID-WN10-UR-000160-Remediation.ps1
-#>
-
 # Enable verbose output
 $VerbosePreference = "Continue"
 
