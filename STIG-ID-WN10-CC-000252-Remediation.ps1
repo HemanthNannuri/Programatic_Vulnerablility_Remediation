d44656230d22596130fea5876b52821421d7e3d2
Hemanth Kumar Nannuri <nannurihk007@gmail.com>
2025-06-09 15:58:28 -0500

    Update STIG-ID-WN10-CC-000252-Remediation.ps1

--- STIG-ID-WN10-CC-000252-Remediation.ps1
+++ STIG-ID-WN10-CC-000252-Remediation.ps1
@@ -1,29 +1,3 @@
-<#
-.SYNOPSIS
-    This PowerShell script ensures that Windows 10 must be configured to disable Windows Game Recording and Broadcasting. Windows Game Recording and Broadcasting is intended for use with games, however it could potentially record screen shots of other applications and expose sensitive data. Disabling the feature will prevent this from occurring.
-.NOTES
-    Author          : Carlton Hurd
-    LinkedIn        : linkedin.com/in/carlton-hurd-6069a5120/
-    GitHub          : github.com/churd-git
-    Date Created    : 2025-01-14
-    Last Modified   : 2025-01-14
-    Version         : 1.0
-    CVEs            : N/A
-    Plugin IDs      : N/A
-    STIG-ID         : WN10-CC-000252
-
-.TESTED ON
-    Date(s) Tested  : 2025-01-14
-    Tested By       : Carlton Hurd
-    Systems Tested  : 
-    PowerShell Ver. : 
-
-.USAGE
-    Please download the script and execute as administrator. 
-    Example syntax:
-    PS C:\> .\STIG-ID-WN10-CC-000252-Remediation.ps1
-#>
-
 # Enable verbose output
 $VerbosePreference = "Continue"
 
