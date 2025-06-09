6c73f426f323f5a097be56db3711d6b9ea5ce103
Hemanth Kumar Nannuri <nannurihk007@gmail.com>
2025-06-09 16:03:09 -0500

    Update STIG-ID-WN10-UC-000015-Remediation.ps1

--- STIG-ID-WN10-UC-000015-Remediation.ps1
+++ STIG-ID-WN10-UC-000015-Remediation.ps1
@@ -1,28 +1,3 @@
-<#
-.SYNOPSIS
-    This PowerShell script ensures that Toast notifications to the lock screen must be turned off. Toast notifications that are displayed on the lock screen could display sensitive information to unauthorized personnel. Turning off this feature will limit access to the information to a logged on user.
-.NOTES
-    Author          : Carlton Hurd
-    LinkedIn        : linkedin.com/in/carlton-hurd-6069a5120/
-    GitHub          : github.com/churd-git
-    Date Created    : 2025-01-14
-    Last Modified   : 2025-01-14
-    Version         : 1.0
-    CVEs            : N/A
-    Plugin IDs      : N/A
-    STIG-ID         : WN10-UC-000015
-
-.TESTED ON
-    Date(s) Tested  : 2025-01-14
-    Tested By       : Carlton Hurd
-    Systems Tested  : 
-    PowerShell Ver. : 
-
-.USAGE
-    Please download the script and execute as administrator. 
-    Example syntax:
-    PS C:\> .\STIG-ID-WN10-UC-000015-Remediation.ps1
-#>
 
 # Enable verbose output
 $VerbosePreference = "Continue"
