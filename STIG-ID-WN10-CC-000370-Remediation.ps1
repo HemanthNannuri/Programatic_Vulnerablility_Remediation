c1275a92a637f607b8120bc9fc2fd825a3f6f4ba
Hemanth Kumar Nannuri <nannurihk007@gmail.com>
2025-06-09 16:00:26 -0500

    Update STIG-ID-WN10-CC-000370-Remediation.ps1

--- STIG-ID-WN10-CC-000370-Remediation.ps1
+++ STIG-ID-WN10-CC-000370-Remediation.ps1
@@ -1,28 +1,3 @@
-<#
-.SYNOPSIS
-    This PowerShell script ensures that The convenience PIN for Windows 10 must be disabled. This policy controls whether a domain user can sign in using a convenience PIN to prevent enabling (Password Stuffer).
-.NOTES
-    Author          : Carlton Hurd
-    LinkedIn        : linkedin.com/in/carlton-hurd-6069a5120/
-    GitHub          : github.com/churd-git
-    Date Created    : 2025-01-14
-    Last Modified   : 2025-01-14
-    Version         : 1.0
-    CVEs            : N/A
-    Plugin IDs      : N/A
-    STIG-ID         : WN10-CC-000370
-
-.TESTED ON
-    Date(s) Tested  : 2025-01-14
-    Tested By       : Carlton Hurd
-    Systems Tested  : 
-    PowerShell Ver. : 
-
-.USAGE
-    Please download the script and execute as administrator. 
-    Example syntax:
-    PS C:\> .\STIG-ID-WN10-CC-000370-Remediation.ps1
-#>
 
 # Enable verbose output
 $VerbosePreference = "Continue"
